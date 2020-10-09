82ce1db3bb6635b463ccc3d9428baa9001f33edf
Idachi Rufusy <idachirufus@gmail.com>
2020-10-09 17:58:57 +0300

    withdraw

--- src/main/java/com/rafikibora/gateway/iso/HttpClient.java
+++ src/main/java/com/rafikibora/gateway/iso/HttpClient.java
@@ -13,24 +13,39 @@ public class HttpClient {
     /* Returns response status as a String - Ok or otherwise
 
      */
-    public String post (String url, HashMap data) {
+//    public String post (String url, HashMap data) {
+//        RestTemplate httpClient = new RestTemplate();
+//
+//        String result = null;
+//
+//        try {
+//            result = httpClient.postForObject(url,
+//                    data,
+//                    String.class);
+//
+//            result = result.trim();
+//        } catch (Exception ex) {
+//            Logger.getLogger(HttpClient.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//
+//        return  result;
+//    }
+
+    public HashMap post (String url, HashMap data) {
         RestTemplate httpClient = new RestTemplate();
 
-        String result = null;
+        HashMap result = null;
 
         try {
             result = httpClient.postForObject(url,
                     data,
-                    String.class);
-
-            result = result.trim();
+                    HashMap.class);
         } catch (Exception ex) {
             Logger.getLogger(HttpClient.class.getName()).log(Level.SEVERE, null, ex);
         }
 
         return  result;
     }
-
 
     // Returns response data as a Map object
     public HashMap get (String url) {
